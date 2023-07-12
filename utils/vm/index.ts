c79bc57a0352f67056542600d02e3986028191ce
SugitaMotoki <ce235019@meiji.ac.jp>
2023-07-12 13:02:32 +0900

    vmを自動インポート

--- utils/vm/index.ts
+++ utils/vm/index.ts
@@ -1,11 +1,20 @@
 /** 第1回発表（2023/05/26） */
-export * as vm01 from "./vm01";
+import * as vm01 from "./vm01";
 
 /** 第2回発表（2023/06/09） */
-export * as vm02 from "./vm02";
+import * as vm02 from "./vm02";
 
 /** 第3回発表（2023/06/24） */
-export * as vm03 from "./vm03";
+import * as vm03 from "./vm03";
 
 /** 第4回発表（2023/07/07） */
-export * as vm04 from "./vm04";
+import * as vm04 from "./vm04";
+
+export const vm = () => {
+  return {
+    vm01,
+    vm02,
+    vm03,
+    vm04,
+  }
+};
