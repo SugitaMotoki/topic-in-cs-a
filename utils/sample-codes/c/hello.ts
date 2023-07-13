a64d47352c3ab09532d33a7e9f62ac0e7affaeed
SugitaMotoki <ce235019@meiji.ac.jp>
2023-07-13 12:20:57 +0900

    Cとアセンブリのサンプルコードを追加

---
+++ utils/sample-codes/c/hello.ts
@@ -0,0 +1,7 @@
+export const hello = `\
+#include <stdio.h>
+int main(void) {
+  printf("Hello World!\\n");
+  return 0;
+}
+`;
---
+++ utils/sample-codes/index.ts
@@ -0,0 +1,9 @@
+import * as c from "./c";
+import * as assembly from "./assembly";
+
+export const sampleCodes = () => {
+  return {
+    c,
+    assembly,
+  };
+};
